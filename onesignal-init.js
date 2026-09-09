5ffed444dca0266260ac4af577f456c04367dd49
WindowsXP2005 <perkinsn2005@gmail.com>
2026-09-08 20:32:39 -0500

    Update onesignal-init.js

--- onesignal-init.js
+++ onesignal-init.js
@@ -9,7 +9,7 @@
 window.OneSignalDeferred = window.OneSignalDeferred || [];
 OneSignalDeferred.push(async function (OneSignal) {
   await OneSignal.init({
-    appId: "YOUR-ONESIGNAL-APP-ID-HERE",
+    appId: "6148eb7c-5d9a-4b9a-996b-a9da1bc5d89b",
     notifyButton: {
       enable: true, // shows a small bell icon users can click to subscribe
     },
